07850cfa2f2acd96422ed383af96ba3aca926500
Olawuyi Ayansola <codedev85@gmail.com>
2022-02-14 13:00:39 +0100

    fierst commit

---
+++ app/Http/Controllers/Api/CustomerCrm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerCrm extends Controller
+{
+    public function findClient(Request $request)
+    {
+        $request->validate([
+            'phone_number' => $request
+        ]);
+
+        $findCustomer = Customer::where('phone_number' , $request->phone_number)->first();
+
+        if(!$findCustomer)
+        {
+            return response()->json(['success' => false , 'message' => 'Could not find hte customer']);
+        }
+
+
+        return response()->json(['success' => true , 'message' => 'Customer found' , 'data' => compact('findCustomer')]);
+
+
+    }
+}
---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Customer::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'full_name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address,
+            'phone_number' => $this->faker->phoneNumber,
+            'company_name' => $this->faker->company,
+        ];
+    }
+}
